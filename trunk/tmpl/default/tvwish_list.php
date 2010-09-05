fa0f0c2d69e243d371294eae2cbb5b0232132fa5
chadopp <chadopp@c2fb60d7-ea4e-fad6-e7d8-52bb8738f97b>
2010-09-05 00:35:46 +0000

    Initial checkin

---
+++ trunk/tmpl/default/tvwish_list.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * tvwish listing
+ *
+ * @date        $Date: 2010-08-01 $
+ * @version     $Revision: 1.0 $
+ * @author      $Author: coppliger $
+ * @license     $GPL $
+ *
+/**/
+
+// Set the desired page title
+$page_title = 'MythWeb - '.t('TV Wish');
+
+// Print the page header
+require 'modules/_shared/tmpl/'.tmpl.'/header.php';
+
+global $activatedShow, $deactivatedShow, $wishFiles;
+
+?>
+
+<form name="tvwish" class="form" method="post" action="tvwish_list">
+<table width="100%" border="0" cellpadding="4" cellspacing="2" class="list small">
+<tr class="menu" align="center">
+   <td>Master TVwish listing file</td>
+</tr>
+</table>
+
+<table width="100%" border="0" cellpadding="4" cellspacing="2" class="list small">    
+<?php
+foreach ($activatedShow as $activeShow) {
+    if (eregi("^(Include)", $activeShow)) {
+        $activeShow = ltrim($activeShow, "Include: $tvwishep/");
+?>
+<tr class="settings" align="left">
+   <td width=15%><a href="episode/tvwish_list?wishstr=deactivate&setting=<?php echo $activeShow ?>"><?php echo Deactivate?></a></td>
+   <td><?php echo $activeShow ?></td>   
+</tr>
+
+    <?php
+    }
+}
+    ?>
+</table>
+
+<table width="100%" border="0" cellpadding="4" cellspacing="2" class="list small">
+<tr class="menu" align="center">
+   <td>Available TVwish lists</td>
+</tr>
+</table>
+
+<table width="100%" border="0" cellpadding="4" cellspacing="2" class="list small">
+<?php
+foreach ($wishFiles as $deactiveShow) {
+    if (!in_array($deactiveShow, $deactivatedShow)) {
+?>
+
+<tr class="settings" align="left">
+   <td width=15%><a href="episode/tvwish_list?wishstr=activate&setting=<?php echo "Include: $tvwishep/$deactiveShow" ?>"><?php echo Activate?></a></td>
+   <td><?php echo "$deactiveShow" ?></td>
+</tr>
+
+    <?php
+    }
+}
+    ?>
+
+</table>
+</form>
+
+<?php
+// Print the page footer
+    require 'modules/_shared/tmpl/'.tmpl.'/footer.php';
+?>
